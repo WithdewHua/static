d32f8b901702362e72b171b55e189144b969076b
WithdewHua <xp724110782@gmail.com>
2019-04-30 21:32:38 +0800

    update sw

--- sw.js
+++ sw.js
@@ -22,116 +22,90 @@ self.addEventListener("unhandledrejection", function(e) {
   });
 });
 
-importScripts("https://g.alicdn.com/kg/workbox/3.3.0/workbox-sw.js");
+importScripts("https://cdn.jsdelivr.net/npm/workbox-cdn@3.6.3/workbox/workbox-sw.js");
 workbox.setConfig({
   debug: false,
-  modulePathPrefix: "https://g.alicdn.com/kg/workbox/3.3.0/"
+  modulePathPrefix: "https://cdn.jsdelivr.net/npm/workbox-cdn@3.6.3/workbox/"
 });
 workbox.skipWaiting();
 workbox.clientsClaim();
 
-var cacheList = ["/"];
+const maxEntries = 100;
 
 workbox.routing.registerRoute(
-  function(event) {
-    // 需要缓存的HTML路径列表
-    if (event.url.host === "withdewhua.space") {
-      if (~cacheList.indexOf(event.url.pathname)) return true;
-      else return false;
-    } else {
-      return false;
-    }
-  },
+  new RegExp(/https:\/\/withdewhua\.space\/.*/),
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: "fcj:blog",
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries,
+        maxAgeSeconds: 30 * 24 * 60 * 60
+      }),
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200]
+      })
+    ]
+  })
+);
+
+// 图片、样式表、字体文件等
+workbox.routing.registerRoute(
+  // css/js
+  new RegExp(/.*\.(css|js)/),
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: "fcj:static",
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries
+      })
+    ]
+  })
+);
+
+workbox.routing.registerRoute(
+  // img
+  new RegExp(/.*\.(?:png|jpg|jpeg|svg|gif)/),
+  workbox.strategies.cacheFirst({
+    cacheName: "fcj:img",
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200]
+      }),
+      new workbox.expiration.Plugin({
+        maxEntries,
+        maxAgeSeconds: 12 * 60 * 60
+      })
+    ]
+  })
+);
+
+workbox.routing.registerRoute(
+  // Cache Fonts files
+  /.*\.(woff|woff2)/,
+  workbox.strategies.cacheFirst({
+      cacheName: 'fcj:font',
+      plugins: [
+          // 使用 expiration 插件实现缓存条目数目和时间控制
+          new workbox.expiration.Plugin({
+              // 最大保存项目
+              maxEntries,
+              // 缓存 30 天
+              maxAgeSeconds: 30 * 24 * 60 * 60,
+          }),
+          // 使用 cacheableResponse 插件缓存状态码为 0 的请求
+          new workbox.cacheableResponse.Plugin({
+              statuses: [0, 200],
+          }),
+      ]
+  })
+);
+
+// 其他的默认规则
+workbox.routing.setDefaultHandler(
   workbox.strategies.networkFirst({
-    cacheName: "fcj:html",
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 10
-      })
-    ]
-  })
-);
-
-workbox.routing.registerRoute(
-  new RegExp(/https:\/\/cdn\.jsdelivr\.net\/((?!img).)*/),
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: "fcj:static",
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 20
-      })
-    ]
-  })
-);
-
-workbox.routing.registerRoute(
-  new RegExp("https://cdn1\.lncld\.net/"),
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: "fcj:static",
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 20
-      })
-    ]
-  })
-);
-
-workbox.routing.registerRoute(
-  new RegExp(/https:\/\/(\w)+\.alicdn\.com\//),
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: "fcj:static",
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 20
-      })
-    ]
-  })
-);
-
-workbox.routing.registerRoute(
-  new RegExp("https://pic\.superbed\.cn/"),
-  workbox.strategies.cacheFirst({
-    cacheName: "fcj:img",
-    plugins: [
-      new workbox.cacheableResponse.Plugin({
-        statuses: [0, 200]
-      }),
-      new workbox.expiration.Plugin({
-        maxEntries: 20,
-        maxAgeSeconds: 12 * 60 * 60
-      })
-    ]
-  })
-);
-
-workbox.routing.registerRoute(
-  new RegExp(/https:\/\/(\w)+\.loli\.net/),
-  workbox.strategies.cacheFirst({
-    cacheName: "fcj:img",
-    plugins: [
-      new workbox.cacheableResponse.Plugin({
-        statuses: [0, 200]
-      }),
-      new workbox.expiration.Plugin({
-        maxEntries: 30,
-        maxAgeSeconds: 12 * 60 * 60
-      })
-    ]
-  })
-);
-
-workbox.routing.registerRoute(
-  new RegExp(/https:\/\/cdn\.jsdelivr\.net\/gh\/WithdewHua\/static@withdewhua-hugo\/img\//),
-  workbox.strategies.cacheFirst({
-    cacheName: "fcj:img",
-    plugins: [
-      new workbox.cacheableResponse.Plugin({
-        statuses: [0, 200]
-      }),
-      new workbox.expiration.Plugin({
-        maxEntries: 30,
-        maxAgeSeconds: 12 * 60 * 60
-      })
-    ]
+      options: [{
+          // 超过 3s 请求没有响应则 fallback 到 cache
+          networkTimeoutSeconds: 3,
+      }]
   })
 );
